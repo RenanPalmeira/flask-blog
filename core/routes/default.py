9c72b6f139e8ce92c80a3501c4bfb37fab94df30
Renan <tomrpg44@gmail.com>
2015-02-10 23:50:49 -0200

    little details#2

--- core/routes/default.py
+++ core/routes/default.py
@@ -15,6 +15,16 @@ def default():
 	post=Post.query.filter_by(blog_id=response['id_blog'],status=1).all()
 	return render_template("index.html",blog=response,posts=post)
 
+@app.route("/<int:id_post>")
+def read_profile(id_post):
+	blog = Routes.session_blog()
+	post = Post.query.filter_by(id_post=id_post, blog_id=blog['id_blog'], status=1)
+	if post.count()==1:
+		post=post.first()
+		post.content = Markup(markdown.markdown(post.content))
+		return render_template("post.html", **locals())
+	return redirect(url_for('default.default'))
+
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template_string("Not Found")	
